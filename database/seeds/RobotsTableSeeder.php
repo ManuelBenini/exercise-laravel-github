8fe49f57e5103b2653d6ab8feeeebd2eab1696d1
agostinopique <ago.pique@yahoo.it>
2022-07-01 17:15:54 +0200

    Created db with requested columns; Populated with fake data;

--- database/seeds/RobotsTableSeeder.php
+++ database/seeds/RobotsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Robot;
 
 class RobotsTableSeeder extends Seeder
 {
@@ -9,8 +11,16 @@ class RobotsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 25; $i++){
+            $new_robot = new Robot();
+            $new_robot->name = $faker->domainWord();
+            $new_robot->production_date = $faker->date();
+            $new_robot->description = $faker->text(150);
+            $new_robot->robot_id = $faker->regexify('[A-Z]{5}[0-9]{4}');
+            $new_robot->nuke_Available = $faker->boolean();
+            $new_robot->save();
+        }
     }
 }
